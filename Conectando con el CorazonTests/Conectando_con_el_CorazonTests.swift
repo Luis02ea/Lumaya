d8426ced344523a3f84e24053d926c98148f025c
Luis Enrique Alcantar Garcia <121569285+Luis02ea@users.noreply.github.com>
2025-03-29 12:31:58 -0600

    Add files via upload

---
+++ Conectando con el CorazonTests/Conectando_con_el_CorazonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Conectando_con_el_CorazonTests.swift
+//  Conectando con el CorazonTests
+//
+//  Created by Alumno on 28/03/25.
+//
+
+import Testing
+@testable import Conectando_con_el_Corazon
+
+struct Conectando_con_el_CorazonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
